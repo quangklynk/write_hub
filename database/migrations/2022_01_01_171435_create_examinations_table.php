494704e9c557af9dcee6da45afd4d24ecbe9c1ca
Quang Trương <quangklynk@gmail.com>
2022-01-02 00:28:18 +0700

    ok1

---
+++ database/migrations/2022_01_01_171435_create_examinations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExaminationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('examinations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('dateExam');
+            $table->unsignedInteger('idClass');
+            $table->unsignedInteger('idTeacher');
+            $table->unique(array('dateExam', 'idClass', 'idTeacher'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('examinations');
+    }
+}
